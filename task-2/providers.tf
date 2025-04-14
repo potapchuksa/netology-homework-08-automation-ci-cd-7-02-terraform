4c415bd1f55363772e3294a5d427ab0cd9c0ef4c
PotapchukSA <potapchuk.sa@yandex.by>
2025-04-14 15:21:35 +0000

    Task 2 solved

---
+++ task-2/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">=1.8.4"
+
+  required_providers {
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
